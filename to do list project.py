5de687421e8460d9e6db7be4fb6fd0650c74f43f
kemi <150168585+kem-codes@users.noreply.github.com>
2024-02-04 18:29:34 +0100

    to do list project

---
+++ to do list project.py
@@ -0,0 +1,17 @@
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.title=("To-Do-List")
+root.geometry("400x650+400+100")
+root.resizable(False,False)
+
+task_list = []
+
+#icon
+image_icon=PhotoImage(file="Image/task.png")
+root.iconphoto(False,Image_icon)
+
+#top bar
+
+root.mainloop()
